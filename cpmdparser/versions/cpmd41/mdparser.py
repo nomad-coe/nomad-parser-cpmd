1863cbd46536a3fb185a49d002ed8c250dd69572
Markus Scheidgen <markus.scheidgen@gmail.com>
2020-03-19 12:22:36 +0100

    Added metainfo python code.

--- cpmdparser/versions/cpmd41/mdparser.py
+++ cpmdparser/versions/cpmd41/mdparser.py
@@ -1,11 +1,11 @@
 # Copyright 2016-2018 Lauri Himanen, Fawzi Mohamed, Ankit Kariryaa
-# 
+#
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
 #   You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #   Unless required by applicable law or agreed to in writing, software
 #   distributed under the License is distributed on an "AS IS" BASIS,
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -208,7 +208,7 @@ class CPMDMDParser(MainHierarchicalParser):
                             self.backend.addArrayValues("atom_velocities", velocities, unit="bohr/(hbar/hartree)")
 
                             forces = values[:, 7:10]
-                            self.backend.addArrayValues("atom_forces", forces, unit="forceAu")
+                            self.backend.addArrayValues("atom_forces", forces, unit="hartree / bohr")
 
                             if trajec_file_iterator is None:
                                 pos = values[:, 1:4]
